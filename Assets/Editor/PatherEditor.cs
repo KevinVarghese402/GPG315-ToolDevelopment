1b6a513971e9c38962f89966e91ae405a10ec2af
Kevin.V <1025532@student.sae.edu.au>
2025-06-28 22:10:02 +1000

    Initial commit

---
+++ Assets/Editor/PatherEditor.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(Path))]
+public class PatherEditor : Editor
+{
+    Vector3 pos = Vector3.zero;
+    private void OnSceneGUI()
+    {
+        
+    }
+}
---
+++ Assets/scrtips/HelloWorldWindow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEditor;
+
+public class HelloWorldWindow : EditorWindow
+{
+    [MenuItem("Window/Hello World")]
+    public static void ShowWindow()
+    {
+        GetWindow<HelloWorldWindow>("Hello World");
+    }
+
+    void OnGUI()
+    {
+        // Create a custom GUIStyle for the label
+        GUIStyle titleStyle = new GUIStyle(EditorStyles.boldLabel);
+        titleStyle.fontSize = 30;
+        titleStyle.normal.textColor = Color.cyan;
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+        titleStyle.margin = new RectOffset(0, 0, 20, 20);
+        
+        GUILayout.Label("Hello World!", titleStyle, GUILayout.ExpandWidth(true));
+
+        GUI.backgroundColor = Color.black;
+
+        
+        if (GUILayout.Button("Close", GUILayout.Width(150), GUILayout.Height(40)))
+        {
+            this.Close();
+        }
+
+        // Reset the background color after the button
+        GUI.backgroundColor = Color.white;
+    }
+}
